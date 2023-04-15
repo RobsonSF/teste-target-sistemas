7b6577fa6a1bca93379b9c681b060bc6e028349f
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-15 00:35:18 -0300

    :sparkles: feat: Colors created

---
+++ challenges/colors/Colors.java
@@ -0,0 +1,8 @@
+package challenges.colors;
+
+public class Colors {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CIANO = "\u001B[36m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+}
